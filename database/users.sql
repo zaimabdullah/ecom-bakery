588b3e3b4b4f7bcb967a9b0e511f5e584a3558a7
Husni Zaim <36534973+zaimabdullah@users.noreply.github.com>
2018-10-15 18:36:56 +0800

    Add files via upload

--- database/users.sql
+++ database/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 01, 2018 at 11:55 AM
+-- Generation Time: Oct 15, 2018 at 12:35 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.2
 
@@ -31,7 +31,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `users` (
   `userID` int(11) NOT NULL,
   `userEmail` varchar(50) NOT NULL,
-  `userPassword` varchar(16) NOT NULL,
+  `userPassword` varchar(255) NOT NULL,
   `userFirstName` varchar(20) NOT NULL,
   `userLastName` varchar(20) NOT NULL,
   `userGender` varchar(5) DEFAULT NULL,
@@ -46,17 +46,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`userID`, `userEmail`, `userPassword`, `userFirstName`, `userLastName`, `userGender`, `userPhone`, `userCity`, `userAddress`, `userStatus`) VALUES
-(1, 'zc14@gmail.com', 'admin', 'husni', 'zaim', 'm', '0000', 'Kota Bharu', 'Kg kkan,Peringat', 2),
-(2, 'ai30@gmail.com', 'admin', 'Aiman', 'Hakeem', 'm', '0000', 'Machang', 'Jln Bahgia, Kg Dahlia', 2),
-(3, 'if@gmail.com', 'admin', 'Khairul', 'Iffan', 'm', '000', 'Kota Bharu', 'Lot 1it, Kubang Kerian', 2),
-(4, 'ar7@gmail.com', 'admin', 'Arif', 'Fikri', 'm', '000', 'Machang', 'Kg Bukit Tiu', 2),
-(15, 'hu@gmail.com', 'user', 'zaim', 'abdullah', 'm', '000', 'Kota Bharu', 'taman bendahara', 1),
-(16, 'siti@gmail.com', 'user', 'siti', 'zubaidah', 'f', '000', 'Kota Bharu', 'Melor', 1),
-(17, 'aaaa@', 'user', 'fikri', 'rusli', 'm', '000', 'Machang', 'Kg Bukitng Kelantan', 1),
-(18, 'abc@gmail.com', 'user', 'khairul', 'iffan', 'm', '12345', 'kb', 'melor', 1),
-(19, 'abcd@gmail.com', 'user', 'khirul', 'ipe', 'm', '2345', 'kb', 'kg bukit tiu', 1),
-(20, 'husni@gmail.com', 'user', 'zaim', 'jim', 'm', '134256', 'machang', 'bukit tiu', 1),
-(21, 'zaim@gmail.com', 'user', 'zaim', 'abdul', 'm', '0000000', 'kb', 'kg situ', 1);
+(27, 'zcreative14@gmail.com', '$2y$10$9HiqAgslodCVfsBg8oAe7uEGdcv.N5jN/26TeO3Iv/1idGX/oxDUG', 'Husni', 'Zaim', 'm', '000', 'KB', 'Per', 1);
 
 --
 -- Indexes for dumped tables
@@ -76,7 +66,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
